7732493482f80fdd868f251c94fa6b6ea58fdc59
Samuel Bangslund <s.bangslund@hotmail.dk>
2020-03-14 17:30:33 +0100

    Added for homework 2 in exercise 01

--- Exercise_01/relfex_agent_with_state.py
+++ Exercise_01/relfex_agent_with_state.py
@@ -28,34 +28,72 @@ should go Right, or Crash, etc. Rerun the agent. Do the Actuators allow bogus ac
     B          Clean   NoOp    B         Clean
     B          Clean   NoOp    B         Clean
     B          Clean   NoOp    B         Clean
+
+Extend the REFLEX-AGENT-WITH-STATE program to
+have 4 locations (4 squares):
+• The agent should only sense and act on the square where it is
+located.
+• Allow any starting square.
+• Use run (20) to test and display results.
+    Current                    New
+    location   status  action  location  status
+    C          Dirty   Suck    C         Clean
+    C          Clean   Left    D         Dirty
+    D          Dirty   Suck    D         Clean
+    D          Clean   Up      A         Dirty
+    A          Dirty   Suck    A         Clean
+    A          Clean   Right   B         Dirty
+    B          Dirty   Suck    B         Clean
+    B          Clean   Down    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
+    C          Clean   NoOp    C         Clean
 '''
 
 A = 'A'
 B = 'B'
+C = 'C'
+D = 'D'
 
 state = {}
 action = None
-model = {A: None, B: None}  # Initially ignorant
+model = {A: None, B: None, C: None, D: None}  # Initially ignorant
 
 rule_action = {
     1: 'Suck',
     2: 'Right',
-    3: 'Left',
-    4: 'NoOp'
+    3: 'Down',
+    4: 'Left',
+    5: 'Up',
+    6: 'NoOp'
 }
 
 rules = {
     (A, 'Dirty'): 1,
     (B, 'Dirty'): 1,
+    (C, 'Dirty'): 1,
+    (D, 'Dirty'): 1,
     (A, 'Clean'): 2,
     (B, 'Clean'): 3,
-    (A, B, 'Clean'): 4
+    (C, 'Clean'): 4,
+    (D, 'Clean'): 5,
+    (A, B, C, D, 'Clean'): 6
 }
 
 environment = {
     A: 'Dirty',
     B: 'Dirty',
-    'Current': A
+    C: 'Dirty',
+    D: 'Dirty',
+    'Current': C
 }
 
 
@@ -70,8 +108,8 @@ def reflex_agent_with_state(percept):  # Determine action
 def update_state(state, action, percept):
     (location, status) = percept
     state = percept
-    if model[A] == model[B] == 'Clean':
-        state = (A, B, 'Clean')
+    if model[A] == model[B] == model[C] == model[D] == 'Clean':
+        state = (A, B, C, D, 'Clean')
     model[location] = status
     return state
 
@@ -95,9 +133,13 @@ def actuators(action):  # Modify environment
     if action == 'Suck':
         environment[location] = 'Clean'
     elif action == 'Right' and location == A:
-        environment['Current'] = B
-    elif action == 'Left' and location == B:
-        environment['Current'] = A
+        environment['Current'] = B  # Move to B
+    elif action == 'Down' and location == B:
+        environment['Current'] = C  # Move to C
+    elif action == 'Left' and location == C:
+        environment['Current'] = D  # Move to D
+    elif action == 'Up' and location == D:
+        environment['Current'] = A  # Move to A
 
 
 def run(n):  # Run the agent through n steps
@@ -112,4 +154,4 @@ def run(n):  # Run the agent through n steps
         print("{:8s}{:10s}{:8s}".format(action, location, status))
 
 
-run(10)
+run(20)
