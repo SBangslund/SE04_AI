83d5f8e51e34d035336d2ac5f1368c5ce35b5117
Samuel Bangslund <s.bangslund@hotmail.dk>
2020-03-14 17:23:52 +0100

    Solved for first homework assignment

--- Exercise_01/reflex_vacuum_agent.py
+++ Exercise_01/reflex_vacuum_agent.py
@@ -30,14 +30,46 @@ Actuators allow bogus actions?
     A          Clean   Left    A         Clean
     A          Clean   Left    A         Clean
     A          Clean   Left    A         Clean
+
+Extend the REFLEX-VACUUM-AGENT program to have 4
+locations (4 squares):
+• The agent should only sense and act on the square where it is
+located.
+• Allow any starting square.
+• Use run (20) to test and display results.
+    Current                    New
+    location   status  action  location  status
+    A          Dirty   Suck    A         Clean
+    A          Clean   Right   B         Dirty
+    B          Dirty   Suck    B         Clean
+    B          Clean   Down    C         Dirty
+    C          Dirty   Suck    C         Clean
+    C          Clean   Left    D         Dirty
+    D          Dirty   Suck    D         Clean
+    D          Clean   Up      A         Clean
+    A          Clean   Right   B         Clean
+    B          Clean   Down    C         Clean
+    C          Clean   Left    D         Clean
+    D          Clean   Up      A         Clean
+    A          Clean   Right   B         Clean
+    B          Clean   Down    C         Clean
+    C          Clean   Left    D         Clean
+    D          Clean   Up      A         Clean
+    A          Clean   Right   B         Clean
+    B          Clean   Down    C         Clean
+    C          Clean   Left    D         Clean
 '''
 
 A = 'A'
 B = 'B'
+C = 'C'
+D = 'D'
 
 environment = {
     A: 'Dirty',
     B: 'Dirty',
+    C: 'Dirty',
+    D: 'Dirty',
     'Current': A
 }
 
@@ -46,9 +78,13 @@ def reflex_vacuum_agent(location_state):  # Determine action
     if location_state[1] == 'Dirty':
         return 'Suck'
     if location_state[0] == A:
-        return 'Left'
-    if location_state[0] == B:
         return 'Right'
+    if location_state[0] == B:
+        return 'Down'
+    if location_state[0] == C:
+        return 'Left'
+    if location_state[0] == D:
+        return 'Up'
 
 
 def sensors():  # Sense environment
@@ -62,8 +98,12 @@ def actuators(action):  # Modify environment
         environment[location] = 'Clean'  # Clean current location
     elif action == 'Right' and location == A:
         environment['Current'] = B  # Move to B
-    elif action == 'Left' and location == B:
-        environment['Current'] = A  # Move to A
+    elif action == 'Down' and location == B:
+        environment['Current'] = C  # Move to A
+    elif action == 'Left' and location == C:
+        environment['Current'] = D
+    elif action == 'Up' and location == D:
+        environment['Current'] = A
 
 
 def run(n):  # Run the agent through n steps
@@ -78,4 +118,4 @@ def run(n):  # Run the agent through n steps
         print("{:8s}{:10s}{:8s}".format(action, location, status))
 
 
-run(10)
+run(20)
